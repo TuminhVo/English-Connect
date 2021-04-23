3ed3498e7f5edd19cb51c089278c22891d007f03
TuminhVo <galaxyskype123@gmail.com>
2021-04-23 20:05:05 +0700

    Learning English

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const toggles = document.getElementsByClassName("collapse-toggle");
-Array.from(toggles).forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.nextElementSibling.classList.toggle("is-collapsed");
-        })
-    });
